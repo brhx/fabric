e5ddfdda555eb3b1456b1ca4289e637f91aea8b4
Brooke Holmes <git@brooke.me>
2026-01-07 08:36:04 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-white text-gray-900">
+      <h1 className="text-2xl font-normal">Hello world</h1>
+    </main>
+  );
+}
+
+export default App;
