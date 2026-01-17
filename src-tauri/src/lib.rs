f45f293b4639837353f08bbddafe03b7a76836ad
Brooke Holmes <git@brooke.me>
2026-01-17 03:34:55 +0000

    Size Tauri window to work area

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,5 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri::Manager;
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -9,6 +10,23 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            if let Some(window) = app.get_webview_window("main") {
+                let monitor = window
+                    .current_monitor()
+                    .ok()
+                    .flatten()
+                    .or_else(|| window.primary_monitor().ok().flatten());
+
+                if let Some(monitor) = monitor {
+                    let work_area = monitor.work_area();
+                    let _ = window.set_position(work_area.position);
+                    let _ = window.set_size(work_area.size);
+                }
+                let _ = window.show();
+            }
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
