d59a9a23275e29d8cfc03eac9c1bfa3d656c8d36
Brooke Holmes <git@brooke.me>
2026-01-23 12:41:19 +0000

    Disable default context menu

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app";
 
+document.addEventListener(
+  "contextmenu",
+  (event) => {
+    event.preventDefault();
+  },
+  { capture: true },
+);
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
